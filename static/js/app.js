f5570b0a83963f349cbbec6de46f3fe7b2540893
Vanessa-Cartagena <vcartagena09@gmail.com>
2022-08-22 14:57:22 -0400

    module 11

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,8 @@
+// import the data from data.js
+const tableData = data;
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
